ff53072700a611902297bd1112b1f9eaeeed0dbd
Shintaro Takemoto <kvrg68@gmail.com>
2024-08-17 19:53:36 +0900

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,32 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  css: ['~/assets/styles/main.scss'],
+  ssr: false,
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  vite:{
+    plugins: [
+      Components({
+        resolvers: [
+          ElementPlusResolver(),
+        
+        ],
+        dts: true,
+        include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      
+      })
+    ]
+  }
+})
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="min-h-screen flex items-center justify-center">
+        <n-button><span>hoge</span></n-button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import {NButton} from 'naive-ui';
+</script>
