a1a8666b07d84777c30721a45c325eb53f9b086d
Shintaro Takemoto <kvrg68@gmail.com>
2024-08-17 22:31:12 +0900

    use TableComponent but something went wrong (video 6)

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,12 +1,41 @@
 <template>
-    <div>
+    <div v-if="users">
         <!-- useFetchメソッドでusersというオブジェクトにJSONが格納されているので、それを出力している-->
-        <pre>{{ JSON.stringify(users, null, 2) }}</pre>
+        <!-- <pre>{{ JSON.stringify(users, null, 2) }}</pre> -->
+        <NDataTable remote :data="users" :colums="colums" />
     </div>
 </template>
 
 <script setup lang="ts">
-import type {User} from "~/models/user";
-const {data: users} = useFetch<User[]>('https://jsonplaceholder.typicode.com/users')
+import { NDataTable, type DataTableColumn } from "naive-ui";
+import type { User } from "~/models/user";
+const { data: users } = useFetch<User[]>('https://jsonplaceholder.typicode.com/users')
+
+
+import type { DataTableColumns } from 'naive-ui'
+
+const colums = computed<DataTableColumns<User>>(() => [
+    {
+        key: 'id',
+        title: 'ID',
+        render: (row) => row.id,
+    },
+    {
+        key: 'name',
+        title: '名前',
+        render: (row) => row.name,
+    },
+    {
+        key: 'email',
+        title: 'Eメール',
+        render: (row) => row.email,
+    },
+    {
+        key: 'website',
+        title: 'ウェブサイト',
+        render: (row) => row.website,
+    }
+])
 </script>
 
+//id name user website
