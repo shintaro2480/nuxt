4b7213ddbc5e68878290391462b1da27b97e06d3
Shintaro Takemoto <kvrg68@gmail.com>
2024-08-17 20:30:16 +0900

    Test API request in users/index vue

---
+++ pages/users/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <!-- useFetchメソッドでusersというオブジェクトにJSONが格納されているので、それを出力している-->
+        <pre>{{ JSON.stringify(users, null, 2) }}</pre>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type {User} from "~/models/user";
+const {data: users} = useFetch<User[]>('https://jsonplaceholder.typicode.com/users')
+</script>
+
