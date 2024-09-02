bd7dfdf0b1bf5a655ce37c9b1f649f1492cdeb93
Shintaro Takemoto <kvrg68@gmail.com>
2024-09-02 18:45:21 +0900

    Fix ElementPlusResolver() to NaiveUiResolver() in nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
     plugins: [
       Components({
         resolvers: [
-          ElementPlusResolver(),
+          NaiveUiResolver(),
         
         ],
         dts: true,
