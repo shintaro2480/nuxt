4aaea9af4ebd6fff7e6473cea42ebcbafd261fdd
Shintaro Takemoto <kvrg68@gmail.com>
2024-09-05 14:37:56 +0900

    video31

---
+++ components/section/user/form.vue
@@ -0,0 +1,19 @@
+<template>
+  <NForm>
+    <div class="flex flex-col gap-[32px]">
+      <NFormItem label="Name">
+        <NInput />
+      </NFormItem>
+      <NFormItem label="Email">
+        <NInput />
+      </NFormItem>
+      <NFormItem label="Phone">
+        <NInput />
+      </NFormItem>
+    </div>
+  </NForm>
+</template>
+
+<script setup lang="ts">
+import { NForm, NFormItem, NInput } from "naive-ui";
+</script>
--- pages/users/new/index.vue
+++ pages/users/new/index.vue
@@ -1,8 +1,23 @@
 <template>
+  <!-- 単なるタイトル -->
+  <div class="px-[40px] py-[32px] flex flex-col gap-[32px]">
+    <span class="text-[24px] font-bold">新規登録</span>
+    <!-- cpomponents/section/user/form内で定義したコンポーネント-->
+    <SectionUserForm />
 
+    <div class="flex flex-row items-center justify-center">
+      <NButton>
+        <span class="text-[14px]">新規登録</span>
+      </NButton>
+    </div>
+
+  </div>
 </template>
 
 <script setup lang="ts">
+import { NButton } from "naive-ui";
+
+//このページには認証が必要である
 definePageMeta({
   requiresAuth: true
 })
